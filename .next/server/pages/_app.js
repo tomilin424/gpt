2eada968ca3c970e17c15f341d99aec0d627793c
tomilin424 <tomilin424@gmail.com>
2024-11-13 22:37:01 +0300

    Initial commit

---
+++ .next/server/pages/_app.js
@@ -0,0 +1,1 @@
+(()=>{var r={};r.id=888,r.ids=[888],r.modules={913:(r,e,t)=>{"use strict";t.r(e),t.d(e,{default:()=>u});var s=t(997);function MyApp({Component:r,pageProps:e}){return s.jsx(r,{...e})}t(764);let u=MyApp},764:()=>{},997:r=>{"use strict";r.exports=require("react/jsx-runtime")}};var e=require("../webpack-runtime.js");e.C(r);var t=e(e.s=913);module.exports=t})();
